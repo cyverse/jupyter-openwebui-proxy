b3b51a0211d45151576055e926edaf4e6dc8278a
Edwin Skidmore <edwins@arizona.edu>
2025-02-06 19:51:18 +0000

    updated rewrite_response to replace /_app and /static

--- jupyter_openwebui_proxy/__init__.py
+++ jupyter_openwebui_proxy/__init__.py
@@ -28,10 +28,10 @@ def rewrite_paths(response):
     '''
     logger.info('rewrite_paths() start')
 
-    # response.body = response.body.replace(b'/_app/immutable/', b'/openwebui/_app/immutable/')
-    # response.body = response.body.replace(b'/favicon/', b'/openwebui/favicon/')
-    # response.body = response.body.replace(b'/opensearch.xml', b'/openwebui/opensearch.xml')
-    # response.body = response.body.replace(b'/static/', b'/openwebui/static/')
+    response.body = response.body.replace(b'/_app/', b'/openwebui/_app/')
+    response.body = response.body.replace(b'/favicon/', b'/openwebui/favicon/')
+    response.body = response.body.replace(b'/opensearch.xml', b'/openwebui/opensearch.xml')
+    response.body = response.body.replace(b'/static/', b'/openwebui/static/')
 
     # for header, v in response.headers.get_all():
     #     if header == "Location":
@@ -67,9 +67,12 @@ def setup_openwebui():
         'timeout': 30,
         'port': 8080,
         'absolute_url': False,
-        # 'rewrite_response': rewrite_paths,
+        'rewrite_response': rewrite_paths,
         'mappath': {
-            '/static/': '/openwebui/static/'
+            '/_app/': '/openwebui/_app/',
+            '/static/': '/openwebui/static/',
+            '/favicon/': '/openwebui/static/',
+            '/opensearch.xml': '/openwebui/opensearch.xml'
         },
         'launcher_entry': {
             'enabled': True,
